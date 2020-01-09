3bb025a340130067214a591ed8c7054c3ceaae92
VincentCSS <VincentHTML@gmail.com>
2020-01-09 09:42:29 -0700

    small edit

--- server/index.js
+++ server/index.js
@@ -1,2 +1,3 @@
-// create a server and route to handle the client get request
+// create a server and route to handle the client get request by sending back
+// data on the requested animal in db.js
 const db = require('./db/db');
