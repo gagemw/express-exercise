4feb63cf18ed032ef1db795ff4bb7b0d8b911367
Gage <gagemw@gmail.com>
2020-02-22 17:12:24 -0700

    Code working express animal server with post route

--- server/index.js
+++ server/index.js
@@ -1,3 +1,29 @@
 // create a server and route to handle the client get request by sending back
 // data on the requested animal in db.js
 const db = require('./db/db');
+const express = require ('express')
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+
+app.get('/:animal', (req,res)=>{
+  let animal = req.params.animal.toLowerCase()
+  res.writeHead(200, {"access-control-allow-origin": "*"})
+  res.write(
+    JSON.stringify({
+        currentAnimal: `${animal}`,
+        currentAnimalPicture: db[`${animal}Picture`],
+        currentAnimalCount: db[`${animal}Count`],
+        inputField: ""
+  }));
+  res.end() 
+})
+
+app.post('/',(req,res)=>{
+  console.log(req.body)
+  Object.assign(db, req.body)
+  res.sendStatus(201)
+})
+
+app.listen(3000, ()=>console.log('server is up and running'))
